de9b9d922b11b5a461bd17c913b466da136188a4
Abdul HaSeeB <eligiable@gmail.com>
2020-06-29 07:21:08 +0400

    Update clearRAM.sh

--- clearRAM.sh
+++ clearRAM.sh
@@ -4,10 +4,10 @@
 #sudo apt install bc
 
 # Get Memory Information
-freemem_before=$(cat /proc/meminfo | grep MemFree | tr -s ' ' | cut -d ' ' -f2) && freemem_before=$(echo "$freemem_before/9216.0"
+freemem_before=$(cat /proc/meminfo | grep MemFree | tr -s ' ' | cut -d ' ' -f2) && freemem_before=$(echo "$freemem_before/1024.0"
 | bc)
 cachedmem_before=$(cat /proc/meminfo | grep "^Cached" | tr -s ' ' | cut -d ' ' -f2) && cachedmem_before=$(echo
-"$cachedmem_before/9216.0" | bc)
+"$cachedmem_before/1024.0" | bc)
 
 # Output Information
 echo -e "This script will clear cached memory and free up your ram.\n\nAt the moment you have $cachedmem_before MiB cached and
@@ -16,7 +16,7 @@ $freemem_before MiB free memory."
 # Clear Filesystem Buffer using "sync" and Clear Caches
 sync && echo 3 > /proc/sys/vm/drop_caches
 
-freemem_after=$(cat /proc/meminfo | grep MemFree | tr -s ' ' | cut -d ' ' -f2) && freemem_after=$(echo "$freemem_after/9216.0" |
+freemem_after=$(cat /proc/meminfo | grep MemFree | tr -s ' ' | cut -d ' ' -f2) && freemem_after=$(echo "$freemem_after/1024.0" |
 bc)
 
 # Output Summary
